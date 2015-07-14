57799f0ddd0951523e2e30871bd5583fcaf905a4
Michael Wang <michael.wang08@gmail.com>
2015-07-14 18:51:53 -0500

    MASTER Added module, controller, and get for all patients

--- public/core.js
+++ public/core.js
@@ -1,1 +1,16 @@
 //core.js
+
+var Patients = angular.module('Patients', []);
+
+Patients.controller('mainController', ['$scope', '$http', function($scope, $http){
+  $scope.patients = {};
+  
+  $http.get('/api/patients')
+    .success(function(data){
+      $scope.patients = data;
+      console.log('I have patients :', data);
+    })
+    .error(function(data){
+      console.log('ERROR: ', data);
+    });
+}]);
